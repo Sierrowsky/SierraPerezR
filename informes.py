f55af110ee396a59fb1b6e79a06e6079ba7a2c8a
Sierrowsky <13sierras@gmail.com>
2024-06-06 10:51:14 +0200

    Update semi Final

--- informes.py
+++ informes.py
@@ -32,8 +32,8 @@ class informes:
             var.report.setFont('Helvetica-Bold', size=10)
             var.report.drawString(50, 675, str(items[0]))
             var.report.drawString(100, 675, str(items[1]))
-            var.report.drawString(165, 675, str(items[2]))
-            var.report.drawString(250, 675, str(items[3]))
+            var.report.drawString(205, 675, str(items[2]))
+            var.report.drawString(300, 675, str(items[3]))
             var.report.drawString(370, 675, str(items[4]))
             var.report.drawString(470, 675, str(items[5]))
             var.report.line(50, 670, 570, 670)
@@ -59,7 +59,7 @@ class informes:
                 j = 655
                 while query.next():
                     if j <= 80:
-                        var.report.drawString(450, 90, 'Pagina Siguiente')
+                        var.report.drawString(450, 70, 'Pagina Siguiente')
                         var.report.showPage()  # Crear una pagina nueva
                         informes.topInforme(titulo)
                         informes.footInforme(titulo)
@@ -76,7 +76,7 @@ class informes:
                     var.report.drawString(i + 15, j, str(query.value(0)))
                     var.report.drawString(i + 50, j, str(query.value(1)))
                     var.report.drawString(i + 115, j, str(query.value(2)))
-                    var.report.drawString(i + 195, j, str(query.value(3)))
+                    var.report.drawString(i + 245, j, str(query.value(3)))
                     var.report.drawString(i + 320, j, str(query.value(4)))
                     var.report.drawString(i + 420, j, str(query.value(5)))
                     j=j-25
@@ -97,7 +97,7 @@ class informes:
 
             # Asegúrate de que el objeto 'logo' sea de tipo 'PngImageFile'
             if isinstance(logo, Image.Image):
-                var.report.line(50, 800, 525, 800)
+                var.report.line(50, 800, 570, 800)
                 var.report.setFont('Helvetica-Bold', size=14)
                 var.report.drawString(55, 785, 'GamesTeis')
                 var.report.drawString(240, 695, titulo)
@@ -138,7 +138,7 @@ class informes:
 
             # Asegúrate de que el objeto 'logo' sea de tipo 'PngImageFile'
             if isinstance(logo, Image.Image):
-                var.report.line(50, 800, 525, 800)
+                var.report.line(50, 800, 570, 800)
                 var.report.setFont('Helvetica-Bold', size=14)
                 var.report.drawString(55, 785, 'GamesTeis')
                 var.report.drawString(240, 695, titulo)
@@ -178,8 +178,8 @@ class informes:
             nombre = fecha + '_listadoProductos.pdf'
             var.report = canvas.Canvas('informesProductos/' + nombre)
             titulo = 'Listado Productos'
-            informes.topInforme(titulo)
-            informes.footInforme(titulo)
+            informes.topInformeProducto(titulo)
+            informes.footInformeProducto(titulo)
             items = ['idProducto', 'Nombre', 'Precio', 'Stock']
             var.report.setFont('Helvetica-Bold', size=10)
             var.report.drawString(50, 675, str(items[0]))
@@ -195,7 +195,7 @@ class informes:
                 j = 655
                 while query.next():
                     if j <= 80:
-                        var.report.drawString(450, 90, 'Pagina Siguiente')
+                        var.report.drawString(450, 70, 'Pagina Siguiente')
                         var.report.showPage()  # Crear una pagina nueva
                         informes.topInformeProducto(titulo)
                         informes.footInformeProducto(titulo)
@@ -226,74 +226,75 @@ class informes:
     @staticmethod
     def reportFacturas():
         try:
-
-            # Generate file name
-            fecha = datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
-            nombre = f"{fecha}_listadoFactura.pdf"
-            var.report = canvas.Canvas(f'informesFacturas/{nombre}')
-            titulo = 'Listado Productos'
-
-            # Report headers and footers
-            informes.topInforme(titulo)
-            informes.footInforme(titulo)
-
-            # Column titles
-            items = ['idVenta', 'idFactura', 'NombreProducto', 'cantidad', 'precio','total']
-            var.report.setFont('Helvetica-Bold', size=10)
-            var.report.drawString(50, 675, items[0])
-            var.report.drawString(100, 675, items[1])
-            var.report.drawString(150, 675, items[2])
-            var.report.drawString(250, 675, items[3])
-            var.report.drawString(300, 675, items[4])
-            var.report.drawString(350, 675, items[5])
-            var.report.line(50, 670, 570, 670)
-            print(252)
-            # Database query
-            query = QtSql.QSqlQuery()
-
-            query.prepare(
-                "SELECT venta.idVenta, venta.idFactura, producto.nombre, venta.cantidad, venta.precio, "
-                "(venta.cantidad * venta.precio) AS precio_total FROM venta "
-                "INNER JOIN producto ON venta.idProducto = producto.id_producto WHERE idFactura = :dato"
-            )
-            query.bindValue(":dato", var.ui.leCodigoFactura.text())  # You need to provide the value for :dato
-
-            if query.exec():
-
-                i = 55
-                j = 655
-                while query.next():
-                    if j <= 80:
-                        var.report.drawString(450, 90, 'Pagina Siguiente')
-                        var.report.showPage()  # Create a new page
-                        informes.topInformeFactura(titulo)
-                        informes.footInformeFactura(titulo)
-                        var.report.drawString(50, 675, items[0])
-                        var.report.drawString(100, 675, items[1])
-                        var.report.drawString(150, 675, items[2])
-                        var.report.drawString(250, 675, items[3])
-                        var.report.drawString(300, 675, items[4])
-                        var.report.drawString(350, 675, items[5])
-                        var.report.line(50, 625, 570, 670)
-                        i = 55
-                        j = 655
-
-                    var.report.setFont('Helvetica', size=9)
-                    var.report.drawString(50, j, str(query.value(0)))
-                    var.report.drawString(100, j, str(query.value(1)))
-                    var.report.drawString(150, j, str(query.value(2)))
-                    var.report.drawString(250, j, str(query.value(3)))
-                    var.report.drawString(300, j, str(query.value(4)))
-                    var.report.drawString(350, j, str(query.value(5)))
-                    j -= 25
+            if var.ui.leCodigoFactura2.text() is str(""):
+                print("Seleccione una factura")
             else:
-                print(query.lastError().text())
+                # Generate file name
+                fecha = datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
+                nombre = f"{fecha}_listadoFactura.pdf"
+                var.report = canvas.Canvas(f'informesFacturas/{nombre}')
+                titulo = 'Listado Productos'
 
-            var.report.save()
-            rootPath = './informesFacturas'
-            for file in os.listdir(rootPath):
-                if file.endswith(nombre):
-                    os.startfile(os.path.join(rootPath, file))
+                # Report headers and footers
+                informes.topInformeFactura(titulo)
+                informes.footInformeFactura(titulo)
+
+                # Column titles
+                items = ['IdVenta', 'IdFactura', 'NombreProducto', 'Cantidad', 'Precio','Total']
+                var.report.setFont('Helvetica-Bold', size=10)
+                var.report.drawString(50, 635, str(items[0]))
+                var.report.drawString(100, 635, str(items[1]))
+                var.report.drawString(275, 635, str(items[2]))
+                var.report.drawString(400, 635, str(items[3]))
+                var.report.drawString(470, 635, str(items[4]))
+                var.report.drawString(520, 635, str(items[5]))
+                var.report.line(50, 630, 570, 630)
+                print(252)
+                # Database query
+                query = QtSql.QSqlQuery()
+
+                query.prepare(
+                    "SELECT venta.idVenta, venta.idFactura, producto.nombre, venta.cantidad, venta.precio, "
+                    "(venta.cantidad * venta.precio) AS precio_total FROM venta "
+                    "INNER JOIN producto ON venta.idProducto = producto.id_producto WHERE idFactura = :dato"
+                )
+                query.bindValue(":dato", var.ui.leCodigoFactura.text())  # You need to provide the value for :dato
+
+                if query.exec():
+
+                    i = 55
+                    j = 615
+                    while query.next():
+                        if j <= 80:
+                            var.report.drawString(450, 70, 'Pagina Siguiente')
+                            var.report.showPage()  # Crear una pagina nueva
+                            informes.topInformeFactura(titulo)
+                            informes.footInformeFactura(titulo)
+                            var.report.drawString(50, 615, str(items[0]))
+                            var.report.drawString(100, 615, str(items[1]))
+                            var.report.drawString(275, 615, str(items[2]))
+                            var.report.drawString(400, 615, str(items[3]))
+                            var.report.drawString(470, 615, str(items[4]))
+                            var.report.drawString(520, 615, str(items[5]))
+                            var.report.line(50, 625, 570, 670)
+                            i = 55
+                            j = 615
+                        var.report.setFont('Helvetica', size=9)
+                        var.report.drawString(i + 15, j, str(query.value(0)))
+                        var.report.drawString(i + 50, j, str(query.value(1)))
+                        var.report.drawString(i + 115, j, str(query.value(2)))
+                        var.report.drawString(i + 355, j, str(query.value(3)))
+                        var.report.drawString(i + 420, j, str(query.value(4)))
+                        var.report.drawString(i + 465, j, str(query.value(5)))
+                        j = j - 25
+                else:
+                    print(query.lastError().text())
+
+                var.report.save()
+                rootPath = './informesFacturas'
+                for file in os.listdir(rootPath):
+                    if file.endswith(nombre):
+                        os.startfile(os.path.join(rootPath, file))
         except Exception as error:
             print("Error informesFacturas: ", error)
 
@@ -308,24 +309,21 @@ class informes:
             fecha = datetime.today().strftime('%d/%m/%Y')
 
             if isinstance(logo, Image.Image):
-                var.report.line(50, 800, 525, 800)
+                var.report.line(50, 800, 570, 800)
                 var.report.setFont('Helvetica-Bold', size=14)
-                var.report.drawString(55, 785, 'Games Teis')
-                var.report.drawString(240, 670, titulo)
-                var.report.line(50, 665, 525, 665)
-                var.report.drawImage(ruta_logo, 480, 725, width=40, height=40)
+                var.report.drawString(55, 785, 'GamesTeis')
+                var.report.drawString(240, 665, titulo)
+                var.report.line(50, 660, 570, 660)
 
-                var.report.setFont('Helvetica-Bold', size=8)
-                var.report.drawString(290, 785,
-                                      f'NÚMERO FACTURA: {var.ui.leCodigoFactura.text()}        Fecha: {fecha}')
-                var.report.drawString(290, 770, 'CLIENTE')
+                # Dibuja la imagen en el informe
+                var.report.drawImage(ruta_logo, 480, 725, width=40, height=40)
 
                 var.report.setFont('Helvetica', size=9)
                 var.report.drawString(55, 770, 'CIF: A12345678')
                 var.report.drawString(55, 755, 'Avda. Galicia - 101')
                 var.report.drawString(55, 740, 'Vigo - 36216 - España')
                 var.report.drawString(55, 725, 'Teléfono: 986 132 456')
-                var.report.drawString(55, 710, 'e-mail: cartesteisr@mail.com')
+                var.report.drawString(55, 710, 'e-mail: GamesTeis@mail.com')
 
                 var.report.setFont('Helvetica', size=8)
                 var.report.drawString(290, 755, f'ID: {registro[0]}')
